35b1cce96e39724aa024d94889dfc1061670a915
Ernest-Gray <egray4cs@gmail.com>
2023-03-28 17:48:57 -0400

    Added HttpRequest and Test files

--- app/src/main/java/edu/towson/cosc435/mnrva/HttpRequest.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/HttpRequest.kt
@@ -1,4 +1,30 @@
 package edu.towson.cosc435.mnrva
 
-class Requests {
+import java.net.HttpURLConnection
+import java.net.URL
+
+class HttpRequest {
+
+    companion object{//basicly makes the following functions 'static' so we can do
+        //HttpRequest.doSomeithng instead of making a whole new class every time
+        fun makeRequest(url: String): String{
+
+            //details here: https://developer.android.com/reference/java/net/HttpURLConnection
+            val url = URL(url)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+
+            //some chatgpt assisted code: needs testing
+            try {
+                val inputStream = connection.inputStream
+                val response = inputStream.bufferedReader().use { it.readText() }
+                inputStream.close()
+                print(response)
+                return response
+            } finally {
+                connection.disconnect()
+            }
+            return "Error in request"
+        }
+    }
 }
--- app/src/test/java/edu/towson/cosc435/mnrva/HttpRequestTest.kt
+++ app/src/test/java/edu/towson/cosc435/mnrva/HttpRequestTest.kt
@@ -1,4 +1,14 @@
 package edu.towson.cosc435.mnrva
 
+import org.junit.Assert
+import org.junit.Test
+
 class HttpRequestTest {
+
+    @Test
+    fun simpleHttpTest() {
+        val myUrlTest = "https://www.youtube.com/"
+        val response = HttpRequest.makeRequest(myUrlTest)
+        Assert.assertTrue(response != null)
+    }
 }
