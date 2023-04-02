b3f9690ec76daff92248ffdb79091bb00bbd992d
Alexander Jenkins <ajenki21@students.towson.edu>
2023-04-01 21:49:38 -0400

    Shows the options for the settings screen.

--- app/src/main/java/edu/towson/cosc435/mnrva/ui/nav/MnrvaNavGraph.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/ui/nav/MnrvaNavGraph.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import edu.towson.cosc435.mnrva.ui.settings.SettingsView
 
 @OptIn(ExperimentalComposeApi::class)
 @ExperimentalFoundationApi
@@ -42,7 +43,7 @@ fun MnrvaNavGraph(
 
         // Settings
         composable(Routes.SettingsView.route) {
-            Text("Settings")
+            SettingsView()
         }
 
     }
--- app/src/main/java/edu/towson/cosc435/mnrva/ui/settings/SettingsView.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/ui/settings/SettingsView.kt
@@ -1,2 +1,53 @@
 package edu.towson.cosc435.mnrva.ui.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.towson.cosc435.mnrva.R
+
+@Composable
+fun SettingsView() {
+    Column(modifier = Modifier.padding(12.dp)) {
+
+        // Title
+        Row {
+            Text(
+                stringResource(R.string.settingsHeader), fontSize = 32.sp, fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+
+
+        // Account Settings
+        Row(modifier = Modifier.padding(bottom = 8.dp)) {
+            Column {
+                Text(stringResource(R.string.account), fontSize = 24.sp) // Title
+                Text(stringResource(R.string.accountSettingsDesc)) // Desc
+            }
+        }
+
+        // Notification Settings
+        Row(modifier = Modifier.padding(bottom = 8.dp)) {
+            Column {
+                Text(stringResource(R.string.notifications), fontSize = 24.sp) // Title
+                Text(stringResource(R.string.notificationSettingsDesc)) // Desc
+            }
+        }
+
+        // System Settings
+        Row(modifier = Modifier.padding(bottom = 8.dp)) {
+            Column {
+                Text(stringResource(R.string.system), fontSize = 24.sp) // Title
+                Text(stringResource(R.string.systemSettingsDesc)) // Desc
+            }
+        }
+
+    }
+}
