83958fd2d04903965d5302e28c05d45d722650ac
Ernest Gray <egray14@students.towson.edu>
2023-04-02 01:10:04 -0400

    started calenderview and added import for Kalender

---
+++ app/src/main/java/edu/towson/cosc435/mnrva/ui/calenderView/CalenderView.kt
@@ -0,0 +1,24 @@
+package edu.towson.cosc435.mnrva.ui.calenderView
+
+import androidx.compose.runtime.Composable
+import com.himanshoe.kalendar.Kalendar
+import com.himanshoe.kalendar.model.KalendarEvent
+import com.himanshoe.kalendar.model.KalendarType
+import kotlinx.datetime.LocalDate
+
+
+@Composable
+fun Calender(){
+    var event1 = KalendarEvent(LocalDate(2023, 4,4), "", "")
+
+    var kalenderEvents: List<KalendarEvent> = listOf(event1)
+
+    Kalendar(
+        onCurrentDayClick={kDay,kEvents->
+            println("${kDay.localDate} has the following events: ${kEvents}")
+    },
+        kalendarType=KalendarType.Firey,
+        kalendarEvents = kalenderEvents
+    )
+}
+
