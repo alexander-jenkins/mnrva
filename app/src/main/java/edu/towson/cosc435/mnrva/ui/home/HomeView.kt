99e8c62ac038420c3379eeae735d853d22562a3f
Ernest Gray <egray14@students.towson.edu>
2023-04-02 04:26:58 -0400

    Put TaskCard into its own composable and added date paramter with decent display

--- app/src/main/java/edu/towson/cosc435/mnrva/ui/home/HomeView.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/ui/home/HomeView.kt
@@ -1,5 +1,8 @@
 package edu.towson.cosc435.mnrva.ui.home
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -7,8 +10,17 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.Month
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun HomeView() {
     Box(contentAlignment = Alignment.Center) {
@@ -31,36 +43,63 @@ fun HomeView() {
                 fontSize = 15.sp,
             )
 
-            Card(
-                shape = RoundedCornerShape(5.dp),
-                elevation = 16.dp,
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp, top = 5.dp, bottom = 5.dp)
-                    .fillMaxWidth()
+            //Example Date for the following TaskCard
+            val date = LocalDate.of(2023, Month.APRIL, 2)
+            val time = LocalTime.of(12, 30, 0)
+            val dateTime = LocalDateTime.of(date, time)
+
+            TaskCard(taskName = "Job Interview", dateTime = dateTime , tag = "WORK")
+
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun TaskCard(
+    taskName: String,
+    dateTime: LocalDateTime,
+    tag: String
+){
+    val formatter = DateTimeFormatter.ofPattern("MMMM d, HH:mm", Locale.ENGLISH)
+
+    Card(
+        shape = RoundedCornerShape(5.dp),
+        elevation = 16.dp,
+        modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp, top = 5.dp, bottom = 5.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Column(
+                modifier = Modifier.weight(1.5f)
             ) {
                 Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceEvenly
+                    modifier = Modifier.padding(10.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Column(
-                        modifier = Modifier.weight(1.5f)
-                    ) {
-                        Row(
-                            modifier = Modifier.padding(10.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Text("Job Interview", fontSize = 36.sp,
-                                modifier = Modifier.weight(1.0f))
-                        }
-                        Row(
-                            modifier = Modifier.padding(5.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Text("Artist:", modifier = Modifier.weight(1.0f))
-                        }
-
-                    }
+                    Text("$taskName", fontSize = 36.sp,
+                        modifier = Modifier.weight(1.0f))
                 }
+                Row(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+//                    horizontalArrangement = Arrangement.End
+                ) {
+                    Text("${dateTime.format(formatter)}", modifier = Modifier.weight(1.0f))
+                    Card(
+                        modifier = Modifier.background(Color(25))
+                    ){
+                        Text(tag.toString(), modifier = Modifier.weight(1.0f))
+                    }
+
+                }
+
             }
         }
     }
