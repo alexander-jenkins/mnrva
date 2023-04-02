f2f6fc68a7194b1875406c865e43d096329524ce
Alexander Jenkins <ajenki21@students.towson.edu>
2023-04-01 20:24:47 -0400

    Added buttons for each screen to bottom bar.

--- app/src/main/java/edu/towson/cosc435/mnrva/ui/MainScreen.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/ui/MainScreen.kt
@@ -2,13 +2,17 @@ package edu.towson.cosc435.mnrva.ui
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import edu.towson.cosc435.mnrva.R
 import edu.towson.cosc435.mnrva.ui.nav.MnrvaNavGraph
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalFoundationApi::class)
@@ -17,10 +21,10 @@ fun MainScreen() {
     val nav = rememberNavController()
 
     // Scaffolding for the app
-    Scaffold (
+    Scaffold(
         topBar = { TopBar() },
-        bottomBar = {}
-    ){
+        bottomBar = { BottomBar(nav) }
+    ) {
         MnrvaNavGraph(nav)
     }
 
@@ -29,4 +33,54 @@ fun MainScreen() {
 @Composable
 private fun TopBar() {
     Text(stringResource(R.string.app_name))
+}
+
+@Composable
+private fun BottomBar(
+    navHost: NavHostController,
+) {
+    BottomNavigation(elevation = 16.dp) {
+
+        // Home Button
+        BottomNavigationItem(
+            selected = false,
+            icon = { Icon(Icons.Default.Home, "") },
+            label = { Text("Home") },
+            onClick = {}
+        )
+
+        // Schedule Button
+        BottomNavigationItem(
+            selected = false,
+            icon = { Icon(Icons.Default.List, "") },
+            label = { Text("Schedule") },
+            onClick = {}
+        )
+
+        // Add Item Button
+        BottomNavigationItem(
+            selected = false,
+            icon = { Icon(Icons.Default.Add, "") },
+            label = { Text("New") },
+            onClick = {}
+        )
+
+        // Calendar Button
+        BottomNavigationItem(
+            selected = false,
+            icon = { Icon(Icons.Default.DateRange, "") },
+            label = { Text("Calendar") },
+            onClick = {}
+        )
+
+        // Settings Button
+        BottomNavigationItem(
+            selected = false,
+            icon = { Icon(Icons.Default.Settings, "") },
+            label = { Text("Settings") },
+            onClick = {}
+        )
+
+
+    }
 }
