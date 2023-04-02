445a773fa1055c9be7f33ccc879c0016291c1da9
Ernest Gray <egray14@students.towson.edu>
2023-04-02 03:28:43 -0400

    Refactored some naming conventions and try catch statement

--- app/src/main/java/edu/towson/cosc435/mnrva/HttpRequest.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/HttpRequest.kt
@@ -1,30 +1,34 @@
 package edu.towson.cosc435.mnrva
 
+import android.util.Log
 import java.net.HttpURLConnection
 import java.net.URL
 
 class HttpRequest {
 
-    companion object{//basicly makes the following functions 'static' so we can do
-        //HttpRequest.doSomeithng instead of making a whole new class every time
-        fun makeRequest(url: String): String{
+    companion object {
+        //basically makes the following functions 'static' so we can do
+        //HttpRequest.makeRequest instead of making a whole new class every time
+        fun makeRequest(url_string: String): String {
 
             //details here: https://developer.android.com/reference/java/net/HttpURLConnection
-            val url = URL(url)
-            val connection = url.openConnection() as HttpURLConnection
+            val urlObject = URL(url_string)
+            val connection = urlObject.openConnection() as HttpURLConnection
             connection.requestMethod = "GET"
 
             //some chatgpt assisted code: needs testing
-            try {
+            return try {
                 val inputStream = connection.inputStream
                 val response = inputStream.bufferedReader().use { it.readText() }
                 inputStream.close()
-                print(response)
-                return response
+                response
+
+            } catch (e: Exception) {
+                Log.d("GET_REQUEST", "error with get request: $e")
+                "HttpRequest Error: $e"
             } finally {
                 connection.disconnect()
             }
-            return "Error in request"
         }
     }
 }
