f01e3187616959059642d40e0d08c616ec1177a4
bbenne13 <bbenne13@students.towson.edu>
2023-04-03 15:20:48 -0700

    Added the correct components to newEntry

--- app/src/main/java/edu/towson/cosc435/mnrva/ui/newEntry/NewEntryView.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/ui/newEntry/NewEntryView.kt
@@ -5,10 +5,11 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -27,90 +28,54 @@ import java.time.Month
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NewEntryView () {
-    val date = LocalDate.of(2023, Month.APRIL, 2)
-    val time = LocalTime.of(12, 30, 0)
-    val dateTime = LocalDateTime.of(date, time)
+    val title : MutableState<String> = remember { mutableStateOf("") }
+    val date : MutableState<String> = remember { mutableStateOf("") }
+    val time : MutableState<String> = remember { mutableStateOf("") }
 
-    TaskCard(taskName = "Job Interview", dateTime = dateTime , tag = "WORK")
-}
+    Box(contentAlignment = Alignment.Center) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            OutlinedTextField(
+                value = title.value,
+                onValueChange = { newValue: String ->
+                    title.value = newValue
+                },
+                label = { Text("Enter Title:") }
+            )
 
-@OptIn(ExperimentalMaterialApi::class)
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun TaskCard(
-    taskName: String,
-    dateTime: LocalDateTime,
-    tag: String
-) {
-    val horizontalGradientBrush = Brush.horizontalGradient(
-        colors = listOf(
-            FringyFlower,
-            Envy
-        )
-    )
-    val formatter = DateTimeFormatter.ofPattern("MMMM d, HH:mm", Locale.ENGLISH)
-    val nav = rememberNavController()
+            OutlinedTextField(
+                value = date.value,
+                onValueChange = { newValue: String ->
+                    date.value = newValue
+                },
+                label = { Text("Enter Date:") }
+            )
 
-    Card(
+            OutlinedTextField(
+                value = time.value,
+                onValueChange = { newValue: String ->
+                    time.value = newValue
+                },
+                label = { Text("Enter Time:") }
+            )
 
-        shape = RoundedCornerShape(15.dp),
-        elevation = 16.dp,
-        onClick = { nav.navigate(Routes.NewEntryView.route) },
-        modifier = Modifier
-            .padding(start = 16.dp, end = 16.dp, top = 5.dp, bottom = 5.dp)
-            .fillMaxWidth()
-    ) {
-        Box(
-            modifier = Modifier
-                .background(brush = horizontalGradientBrush)
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
+            Button(
+                onClick = {},
+                // Uses ButtonDefaults.ContentPadding by default
+                contentPadding = PaddingValues(
+                    start = 20.dp,
+                    top = 12.dp,
+                    end = 20.dp,
+                    bottom = 12.dp
+                )
             ) {
-                Column(
-                    modifier = Modifier.weight(1.5f)
-                ) {
-                    Row(
-                        modifier = Modifier.padding(10.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            "$taskName", fontSize = 36.sp,
-                            modifier = Modifier.weight(1.0f)
-                        )
-                    }
-                    Row(
-                        modifier = Modifier
-                            .padding(5.dp)
-                            .fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-//                    horizontalArrangement = Arrangement.End
-                    ) {
-                        Text("${dateTime.format(formatter)}", modifier = Modifier.weight(1.0f))
-
-                    }
-                    Row(
-                        horizontalArrangement = Arrangement.End
-                    ) {
-                        Spacer(
-                            modifier = Modifier
-                                .padding(5.dp)
-                        )
-
-                        Card(
-                            modifier = Modifier.background(Color(25))
-                        ) {
-                            Text(tag.toString(), modifier = Modifier.weight(1.0f))
-                        }
-                    }
-
-                }
-
+                // Inner content including an icon and a text label
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text("Create")
             }
         }
     }
+
 }
+
