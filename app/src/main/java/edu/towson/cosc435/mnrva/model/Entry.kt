17c04c7326aba0487dc8f8c524a4a6a9eed85cea
Ernest-Gray <egray4cs@gmail.com>
2023-04-11 21:46:10 -0400

    Fixed type of date in Entry.  Started work on jsonifying Entrys for Kalendar

--- app/src/main/java/edu/towson/cosc435/mnrva/model/Entry.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/model/Entry.kt
@@ -1,9 +1,11 @@
 package edu.towson.cosc435.mnrva.model
 
+import kotlinx.datetime.LocalDateTime
+
 data class Entry(
     //TODO: change data types if necessary
    val title: String,
-   val date: String,
+   val date: LocalDateTime,
    val start_time: String,
    val end_time: String,
    val description: String,
--- app/src/main/java/edu/towson/cosc435/mnrva/ui/calenderView/CalendarView.kt
+++ app/src/main/java/edu/towson/cosc435/mnrva/ui/calenderView/CalendarView.kt
@@ -1,15 +1,26 @@
 package edu.towson.cosc435.mnrva.ui.calenderView
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.google.gson.Gson
 import com.himanshoe.kalendar.Kalendar
 import com.himanshoe.kalendar.model.KalendarEvent
 import com.himanshoe.kalendar.model.KalendarType
+import edu.towson.cosc435.mnrva.model.Entry
 import kotlinx.datetime.LocalDate
 
 
 @Composable
 fun Calendar(){
-    val event1 = KalendarEvent(LocalDate(2023, 4,4), "", "")
+    val event1 = KalendarEvent(
+        LocalDate(2023, 4,4),
+        "NAME HERE",
+        "DESC HERE"
+    )
 
     val kalenderEvents: List<KalendarEvent> = listOf(event1)
 
@@ -20,5 +31,47 @@ fun Calendar(){
         kalendarType=KalendarType.Firey,
         kalendarEvents = kalenderEvents
     )
+    Text(text = "List")
+    LazyColumn{
+        items(kalenderEvents){
+            event ->
+            Log.d("LOG", "Hello world $event")
+            val entry: Entry? = parseEntryJson(event.eventDescription)
+            if (entry != null){
+                Column() {
+                    Text(text = entry.title)
+                    Text(text = entry.description)
+                    Text(text = entry.tag)
+                    Text(text = entry.start_time)
+                    Text(text = entry.end_time)
+                    Text(text = entry.date.toString())
+                }
+            }
+            else{
+                Log.d("LOG", "entry didnt parse correctly")
+            }
+        }
+    }
+}
+
+fun entryToKalendarEvent(entry: Entry): KalendarEvent?{
+
+    val date: LocalDate = entry.date.date
+    val eventName = entry.title
+    val eventDesc = entry.description
+
+    return KalendarEvent(date, eventName, eventDesc)
+}
+
+fun parseEntryJson(jsonString: String?): Entry? {
+    val gson: Gson = Gson()
+    try{
+        val myVal = gson.fromJson(jsonString, Entry::class.java)
+        return myVal
+    }
+    catch (e: Exception){
+        Log.d("LOG", e.toString())
+    }
+    return null
 }
 
